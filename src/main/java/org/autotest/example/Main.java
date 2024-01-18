2be27b37f06bbb22ec8c720fa39b75f6c2b70bbc
JimSega <dvigokv@gmail.com>
2024-01-18 16:02:54 +0300

    fix exception, use chromedriver instead of chrome

--- src/main/java/org/autotest/example/Main.java
+++ src/main/java/org/autotest/example/Main.java
@@ -9,9 +9,10 @@ import java.time.Duration;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.setProperty("webdriver.chrome.driver", "/home/ubuntu/java/AmazonTestAuto/chrome-linux64/chrome");
+        System.setProperty("webdriver.chrome.driver", "/home/ubuntu/java/AmazonTestAuto/chromedriver-linux64/chromedriver");
         WebDriver webDriver = new ChromeDriver(new ChromeDriverService.Builder().usingPort(65535).withTimeout(Duration.ofSeconds(10)).build());
         webDriver.get("https://www.amazon.com/");
         System.out.println(webDriver.getTitle());
+        webDriver.close();
     }
 }
